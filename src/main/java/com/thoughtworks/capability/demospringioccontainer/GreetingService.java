e4134831d2caead5d6ee7038b35be47e57d523fb
ZW <zhaowuwei2020@foxmail.com>
2020-11-11 13:43:57 +0800

    [ZWW]: Finish problem 2 solution 1.

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
@@ -1,11 +1,15 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
+import static org.springframework.beans.factory.config.BeanDefinition.SCOPE_PROTOTYPE;
+
 @Service
+@Scope(SCOPE_PROTOTYPE)
 public class GreetingService {
 
     String sayHi() {
-        return "hello world";
+        return "Greeting " + this.hashCode() + ": " + "hello world";
     }
 }
